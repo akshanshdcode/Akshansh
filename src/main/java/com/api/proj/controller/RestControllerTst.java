5bd044e3ec6c7c9f3c092dbd22b3f2df38ab922b
akshanshdcode <akshansh.gupta13@gmail.com>
2023-04-30 12:31:52 +0530

    new commit for testing

---
+++ src/main/java/com/api/proj/controller/RestControllerTst.java
@@ -0,0 +1,12 @@
+package com.api.proj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerTst {
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome to Student Info Application";
+	}
+}
